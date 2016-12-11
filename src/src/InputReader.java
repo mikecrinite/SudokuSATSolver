0b48b005e729be752e76021418bf5c38eeeabb56
mikecrinite <mikecrinite@gmail.com>
2016-12-11 11:59:02 -0500

    Initial commit

---
+++ src/src/InputReader.java
@@ -0,0 +1,32 @@
+import java.io.File;
+
+/**
+ *
+ * @author Michael Crinite
+ * @version 0.1 12/11/2016
+ *
+ */
+public class InputReader {
+
+    private File file;
+    private int[][] puzzle;
+
+    /**
+     * Constructor for Input class.
+     *
+     * @param f File to read
+     */
+    public InputReader(File f) {
+    }
+
+    public int[][] parse(File f){
+
+
+
+
+
+
+
+        return new int[0][0];
+    }
+}
