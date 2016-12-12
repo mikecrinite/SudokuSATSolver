aaf63dec68cf11ead341d8719ae3c23a6a116d90
mikecrinite <mikecrinite@gmail.com>
2016-12-12 00:49:19 -0500

    Cleaned up code: removed lines only usable in development environment, added comments, removed unnecessary whitespace.

--- src/src/Driver.java
+++ src/src/Driver.java
@@ -12,17 +12,20 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
- * Created by Mike on 12/11/2016.
+ * This class retrieves the necessary file from the file system, according to the argument given to it
+ * on the command line. It then calls code from the Translator and InputOutputHandler as well as SAT4J modules
+ * in order to parse, solve, retranslate, and print the solution to the Sudoku puzzle.
+ *
+ * @author Michael Crinite
+ * @version 1.0 12/12/2016
  */
 public class Driver {
     public static void main(String[] args){
         try{
             final long startTime = System.currentTimeMillis();//Take start time
 
-            //if(args.length > 0){
-            if(true){
-                //File file = new File(args[0]);                              // Retrieve the file from the command line
-                File file = new File("C:\\Users\\Mike\\Desktop\\p3.txt");
+            if(args.length > 0){
+                File file = new File(args[0]);                              // Retrieve the file from the command line
                 InputOutputHandler input = new InputOutputHandler(file);    // Pass the file to the IR to parse
                 Translator t = Translator.getInstance();                    // Get an instance of Translator
                 t.setPuzzle(input.parse());                                 // Parse the input and store the puzzle in t
@@ -35,11 +38,11 @@ public class Driver {
                 Reader reader = new DimacsReader(solver);
                 boolean satisfiable = false;
 
-                IProblem problem = reader.parseInstance("C:\\Users\\Mike\\IdeaProjects\\SudokuSATSolver\\src\\files\\sat");
+                //Present puzzle to solver
+                IProblem problem = reader.parseInstance("./sat.txt");
                 satisfiable = problem.isSatisfiable();
 
-
-
+                //Handle puzzle solution or lack thereof
                 if(!satisfiable){//TODO: Add clause for invalid input
                     System.out.println("This puzzle has no valid solution.");
                     System.out.println(solver.unsatExplanation());
@@ -59,7 +62,7 @@ public class Driver {
         }catch(FileNotFoundException e){
             System.out.println("The specified file was not able to be located by SAT4J");
         }catch(IOException e){
-            System.out.println("SudokuSolver cannot read that file, or a valid path was not provided.");
+            System.out.println("The solver cannot read that file, or a valid path was not provided.");
         }catch(ParseFormatException e){
             System.out.println("The parsed file is not in the specified format");
         }catch(ContradictionException e){
